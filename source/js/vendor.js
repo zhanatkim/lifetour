e807b4f56ea1b0cd358047caadb0f1c00c2378f5
zhanatkim <zhanatkim@gmail.com>
2023-07-29 16:03:41 +0600

    Добавляет шрифты и графику

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,4 +1,12 @@
 // Swiper 7.4.1
-import './vendor/swiper';
+import Swiper from './vendor/swiper';
 import './vendor/focus-visible-polyfill';
 
+export const heroSwiper = new Swiper('.main-header__swiper', {
+  spaceBetween: 30,
+  effect: "fade",
+  pagination: {
+    el: '.main-header__pagination',
+    clickable: true,
+  },
+});
