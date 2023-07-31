66a30ec23d4cb0cb8b53db74faeb6a03df3fc69d
zhanatkim <zhanatkim@gmail.com>
2023-08-01 01:58:32 +0600

    Добавляет адаптив хиро, плеер подкаста и видео

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -3,7 +3,8 @@ import Swiper from './vendor/swiper';
 import './vendor/focus-visible-polyfill';
 
 export const heroSwiper = new Swiper('.main-header__swiper', {
-  cssMode: true,
+  // cssMode: true,
+  loop: true,
   pagination: {
     el: '.main-header__pagination',
     clickable: true,
