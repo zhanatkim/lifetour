b74dd49bf60b175c9708073e87afa3362e43ae6f
zhanatkim <zhanatkim@gmail.com>
2023-08-04 00:01:19 +0600

    Добавляет отзывы и о себе

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -31,3 +31,41 @@ export const toursSwiper = new Swiper('.tours__swiper', {
   },
   keyboard: true,
 });
+
+export const trainersSwiper = new Swiper('.training__swiper', {
+  cssMode: true,
+  slidesPerView: 1,
+  spaceBetween: 15,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+  },
+  navigation: {
+    nextEl: '.training__button--next',
+    prevEl: '.training__button--prev',
+  },
+  keyboard: true,
+});
+
+export const reviewSwiper = new Swiper('.review__swiper', {
+  cssMode: true,
+  slidesPerView: 1,
+  spaceBetween: 15,
+  breakpoints: {
+    768: {
+      slidesPerView: 'auto',
+      spaceBetween: 10,
+    },
+  },
+  navigation: {
+    nextEl: '.review__button--next',
+    prevEl: '.review__button--prev',
+  },
+  keyboard: true,
+});
