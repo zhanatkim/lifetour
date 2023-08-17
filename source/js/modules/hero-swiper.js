73b4236eb7f82b1e7edd04746fa20d9a04498494
zhanatkim <zhanatkim@gmail.com>
2023-08-18 03:49:16 +0600

    Вносит последние правки в резину

--- source/js/modules/hero-swiper.js
+++ source/js/modules/hero-swiper.js
@@ -1,11 +1,22 @@
 import Swiper from '../vendor/swiper';
-// import {setEventListenerAudio, setEventListenerVideo} from './media-player';
 import {setEventListener} from './media-player';
 
 // Свайпер для хиро-хедера
+const setInert = () => {
+  const wrapper = document.querySelector('.main-header__wrapper');
+  if (!wrapper) {
+    return;
+  }
+  const focusableSlides = Array.from(wrapper.querySelectorAll('.main-header__slide')).filter((item) => !item.classList.contains('swiper-slide-active'));
+  focusableSlides.forEach((el) => el.setAttribute('inert', true));
+  const activeSlide = document.querySelector('.main-header__slide.swiper-slide-active');
+  activeSlide.removeAttribute('inert');
+};
 
-const heroSwiper = new Swiper('.main-header__swiper', {
+export const heroSwiper = new Swiper('.main-header__swiper', {
+  cssmode: true,
   loop: true,
+  focusableElements: '.main-header__button',
   pagination: {
     el: '.main-header__pagination',
     clickable: true,
@@ -13,20 +24,18 @@ const heroSwiper = new Swiper('.main-header__swiper', {
   on: {
     init: setEventListener(),
   },
+  keyboard: true,
 });
-
+heroSwiper.on('afterInit', setInert());
 heroSwiper.on('slideChange', setEventListener);
+heroSwiper.on('slideChangeTransitionEnd', function () {
+  const slides = document.querySelectorAll('.main-header__slide');
+  const activeSlide = document.querySelectorAll('.main-header__slide.swiper-slide-active');
 
-// const heroSwiper = new Swiper('.main-header__swiper', {
-//   loop: true,
-//   pagination: {
-//     el: '.main-header__pagination',
-//     clickable: true,
-//   },
-//   on: {
-//     init: setEventListenerVideo(),
-//   },
-// });
-
-// heroSwiper.on('slideChange', setEventListenerAudio);
-
+  slides.forEach((slide) => {
+    slide.setAttribute('inert', true);
+  });
+  activeSlide.forEach((slide) => {
+    slide.removeAttribute('inert');
+  });
+});
