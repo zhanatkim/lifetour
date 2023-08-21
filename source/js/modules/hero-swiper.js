691a6c961fede151d28d91a02ba89c65d7f1d849
zhanatkim <zhanatkim@gmail.com>
2023-08-21 21:33:58 +0600

    Самые последние правки после тестирования шапка и футер

--- source/js/modules/hero-swiper.js
+++ source/js/modules/hero-swiper.js
@@ -2,23 +2,33 @@ import Swiper from '../vendor/swiper';
 import {setEventListener} from './media-player';
 
 // Свайпер для хиро-хедера
+
 const setInert = () => {
-  const wrapper = document.querySelector('.main-header__wrapper');
+  const wrapper = document.querySelector('.page-header-swiper__wrapper');
+  const activeSlide = document.querySelector('.page-header-swiper__slide.swiper-slide-active');
+  const bgImages = document.querySelectorAll('.page-header__bg-photo');
+  if (!bgImages) {
+    return;
+  }
+  bgImages.forEach((el) => el.classList.remove('page-header__bg-photo--active'));
+  if (!activeSlide) {
+    return;
+  }
+  const activeImage = Array.from(bgImages).find((item) => item.dataset.index === activeSlide.dataset.swiperSlideIndex);
+  activeImage.classList.add('page-header__bg-photo--active');
   if (!wrapper) {
     return;
   }
-  const focusableSlides = Array.from(wrapper.querySelectorAll('.main-header__slide')).filter((item) => !item.classList.contains('swiper-slide-active'));
+  const focusableSlides = Array.from(wrapper.querySelectorAll('.page-header-swiper__slide')).filter((item) => !item.classList.contains('swiper-slide-active'));
   focusableSlides.forEach((el) => el.setAttribute('inert', true));
-  const activeSlide = document.querySelector('.main-header__slide.swiper-slide-active');
   activeSlide.removeAttribute('inert');
 };
 
-export const heroSwiper = new Swiper('.main-header__swiper', {
+export const heroSwiper = new Swiper('.page-header__swiper', {
   cssmode: true,
   loop: true,
-  focusableElements: '.main-header__button',
   pagination: {
-    el: '.main-header__pagination',
+    el: '.page-header-swiper__pagination',
     clickable: true,
   },
   on: {
@@ -29,13 +39,20 @@ export const heroSwiper = new Swiper('.main-header__swiper', {
 heroSwiper.on('afterInit', setInert());
 heroSwiper.on('slideChange', setEventListener);
 heroSwiper.on('slideChangeTransitionEnd', function () {
-  const slides = document.querySelectorAll('.main-header__slide');
-  const activeSlide = document.querySelectorAll('.main-header__slide.swiper-slide-active');
-
+  const slides = document.querySelectorAll('.page-header-swiper__slide');
+  const activeSlide = document.querySelector('.page-header-swiper__slide.swiper-slide-active');
+  const bgImages = document.querySelectorAll('.page-header__bg-photo');
+  if (!bgImages) {
+    return;
+  }
+  bgImages.forEach((el) => el.classList.remove('page-header__bg-photo--active'));
+  if (!activeSlide) {
+    return;
+  }
+  const activeImage = Array.from(bgImages).find((item) => item.dataset.index === activeSlide.dataset.swiperSlideIndex);
+  activeImage.classList.add('page-header__bg-photo--active');
   slides.forEach((slide) => {
     slide.setAttribute('inert', true);
   });
-  activeSlide.forEach((slide) => {
-    slide.removeAttribute('inert');
-  });
+  activeSlide.removeAttribute('inert');
 });
